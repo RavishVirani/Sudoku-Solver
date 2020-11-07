43796efa29523ea0f4b491c2576e39fc57ba11f4
AndrewChoi001 <72709903+AndrewChoi001@users.noreply.github.com>
2020-11-07 15:16:58 -0500

    Update Cell.py

--- Cell.py
+++ Cell.py
@@ -3,35 +3,42 @@ class Cell:
     def __init__(self,index,value):
         self.relations = []
         self.domain = []
-        #self.relations REAZLLY depends on the implementation of the board
         
         col = index[0]
         row = index[1]
         subCol = 3*((ord(col)- ord("A"))//3)
         subRow = 3*((int(row)-1)//3)
+        
+        #Add all the Column Indices
         for i in range(9):
             if col+str(i+1)!=index:
                 self.relations.append(col+str(i+1))
+        
+        #Add all the Row Indices
         for i in range(9):
             if ALPHA[i]+row!=index:
                 self.relations.append(ALPHA[i]+row)
+        
+        #Add the box indices
         for i in range(3):
             for n in range(3):
                 temp = ALPHA[subCol+i] + str(subRow+1+n)
                 if temp!=index and temp not in self.relations:
                     self.relations.append(temp)
+                    
         if value == ".":
             self.domain = ['1','2','3','4','5','6','7','8','9']
         else:
             self.domain = [value]
             
     def constrain(self,other):
+        # Does arc constraint according to other -> current
+        # Returns True if constraint is done. False otherwise
         if len(other.domain)==1:
             if other.domain[0] in self.domain:
                 self.domain.remove(other.domain[0])
                 return True
-            
-        return True
+        return False
         
         
 class Constraint:
@@ -40,11 +47,5 @@ class Constraint:
         self.other = cellB
     def arc_consist(self):
         return self.current.constrain(self.other)
-
-temp = Cell("A1","2")
-temp1 = Cell("I9",".")
-
-
-
-#print(temp.relations)
-#print(temp.domain)
+    
+    
